9f411fc7d27b63ae7f00c0fa6fbdadd1f5931277
Dr Jason Guo <jason@xing.com.au>
2024-08-06 23:12:14 +1000

    init

---
+++ node_modules/@pega/cosmos-react-core/lib/components/SummaryList/index.d.ts
@@ -0,0 +1,4 @@
+export { default, SummaryListItems, StyledSummaryListItem } from './SummaryList';
+export type { SummaryListProps, SummaryListItem } from './SummaryList';
+export { default as ViewAll } from './ViewAll';
+//# sourceMappingURL=index.d.ts.map
